09cd1369fcfaeced604cb5f56ebafbaa4cd1b5ce
Barnil <barnil2003>
2025-08-05 14:38:31 +0530

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ const MainContent: React.FC = () => {
   return (
     <main className="flex min-h-screen w-[100%] flex-col items-center justify-start py-4 sm:px-6 lg:px-8">
       <Hero/>
+      <Feed/>
     </main>
   );
 };
--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import Card from "./Card";
 const Feed: React.FC = () => {
   return (
-    <div id="feed" className="flex flex-wrap min-h-screen items-center justify-center px-4 sm:px-6 lg:px-8">
-        <Card/>
+    <div id="feed" className="flex flex-col min-h-screen items-center justify-start px-4 sm:px-6 lg:px-8">
+        <h1 className="text-[#ffffff]">YOUR FEED</h1>
+        <div className="flex flex-wrap gap-4 w-full">
+          <Card/>
+        </div>
     </div>
   );
 };
